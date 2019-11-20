26f2ec3fc3a5573f4f36dabfb91ca93dd2dda0a6
Vitor Paes <paesvitor@outlook.com>
2019-11-20 06:03:31 -0300

    Added User model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 const HOST = "0.0.0.0";
 
 const app = express();
